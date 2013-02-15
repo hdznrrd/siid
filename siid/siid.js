d7609fc44fec151b315e0d62c34e7a4baef85b46
hdznrrd <hadez@nrrd.de>
2013-02-15 20:05:22 +0100

    static parameter for testing

--- siid/siid.js
+++ siid/siid.js
@@ -4,7 +4,8 @@ var c = null;
 var DELAY = 2000;
 
 function fetch() {
-	$.get("/siid/apps/powermeter.py", { "n" : $("powergraph_duration").val } ,fetched)
+	$.get("/siid/apps/powermeter.py", "&n=100", fetched)
+	//$.get("/siid/apps/powermeter.py", { "n" : $("powergraph_duration").val } ,fetched)
 }
 
 function fetched(data) {
