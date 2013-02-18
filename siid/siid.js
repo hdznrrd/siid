b3ebf2c43ee39acd64bd5cdbf3c60e1dfb233d27
hdznrrd <hadez@nrrd.de>
2013-02-18 22:39:10 +0100

    support for plotting datacenter graph

--- siid/siid.js
+++ siid/siid.js
@@ -1,13 +1,15 @@
 
 var r = null;
 var c = null;
+var r_datacenter = null;
+var c_datacenter = null;
 var DELAY = 2000;
 
-function fetch() {
-	$.get("/siid/apps/powermeter.py", { n: $("#powergraph_duration").val() }, fetched)
+function fetch_powermeter() {
+	$.get("/siid/apps/powermeter.py", { n: $("#powergraph_duration").val() }, fetched_powermeter)
 }
 
-function fetched(data) {
+function fetched_powermeter(data) {
 	if(c)
 	{
 		c.remove()
@@ -31,11 +33,41 @@ function fetched(data) {
 									}
 									);
 
-	window.setTimeout(fetch,DELAY);
+	window.setTimeout(fetch_powermeter,DELAY);
+}
+
+function fetch_powermeter_datacenter() {
+	$.get("/siid/apps/powermeter_datacenter.py", { n: $("#powergraph_duration").val() }, fetched_powermeter_datacenter)
+}
+
+function fetched_powermeter_datacenter(data) {
+	if(c_datacenter)
+	{
+		c_datacenter.remove()
+	}
+
+	c_datacenter = r_datacenter.linechart(0,
+									0,
+									$("#powergraph_datacenter_only").width()-30,
+									$("#powergraph_datacenter_only").height()-30,
+									data["Seconds ago"],
+									[
+										data["Total"],
+										[0]
+									],
+									{
+										axis: "0 1 1 0",
+										colors: ["#aa00aa"]
+									}
+									);
+
+	window.setTimeout(fetch_powermeter_datacenter,DELAY);
 }
 
 
 function siid_start() {
 	r = new Raphael("powergraph_total");
-	fetch();
+	r_datacenter = new Raphael("powergraph_datacenter_only");
+	fetch_powermeter();
+	fetch_powermeter_datacenter();
 }
